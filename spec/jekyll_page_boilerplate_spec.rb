4f9c0077387753060e4d3b390b8eb4d300c86943
opsaaaaa <sean@ferney.org>
2022-05-10 13:51:12 -0600

    refactor specs for v4.0

--- spec/jekyll_page_boilerplate_spec.rb
+++ spec/jekyll_page_boilerplate_spec.rb
@@ -1,107 +1,93 @@
 RSpec.describe JekyllPageBoilerplate do
+
+  let(:file_content) {File.read('test/title.md')}
+
   it "has a version number" do
     expect(JekyllPageBoilerplate::VERSION).not_to be nil
   end
 
-  context 'page command' do
-    it "can create a new post/page" do
-      output = %x|exe/boilerplate create test|
-      expect(File.exist?('test/title.md')).to eq(true)
-      expect(output).not_to match /Fatal/
-      file_content = ''
-      open('test/title.md', 'r') do |file|
-        file_content = file.read()
-      end
-      expect(file_content).to match /---/
-      expect(file_content).to match /layout: post/
-      expect(file_content).to match /author: John Doe/
+  context '`boilerplate <page>` command' do
+    subject { %x|exe/boilerplate test| }
+  
+    it('creates a file') {expect(File.exist?('test/title.md')).to eq(true)}
+    it('does not throw Fatal') {is_expected.not_to match /Fatal/}
+
+    it 'cant override an existing page' do 
+      subject
+      expect(%x|exe/boilerplate test|).to match /Fatal/
     end
+  end
+
+  context '`boilerplate <page>` file content' do
+    before {%x|exe/boilerplate test|}
+
+    subject {file_content}
     
-    it "cant create two pages with the same name" do
-      output = %x|exe/boilerplate page test|
-      expect(output).to match /Fatal/
+    it "generates correct content" do
+      is_expected.to match /---/
+      is_expected.to match /layout: post/
+      is_expected.to match /author: John Doe/
     end
 
     it 'can create content' do
-      %x|exe/boilerplate page test|
-      file_content = ''
-      open('test/title.md', 'r') do |file|
-        file_content = file.read()
-      end
-      expect(file_content).to match 'some boilerplate text'
-      expect(file_content).to match 'Default Test Heading'
+      is_expected.to match 'some boilerplate text'
+      is_expected.to match 'Default Test Heading'
     end
 
     # _boilerplate:
     #   path: test
     #   timestamp: false
     it 'removes the boilerplate settings' do
-      %x|exe/boilerplate page test|
-      file_content = ''
-      open('test/title.md', 'r') do |file|
-        file_content = file.read()
-      end
-      expect(file_content).not_to match '_boilerplates:'
-      expect(file_content).not_to match 'path: test'
-      expect(file_content).not_to match 'timestamp: false'
+      is_expected.not_to match '_boilerplates:'
+      is_expected.not_to match 'path: test'
+      is_expected.not_to match 'timestamp: false'
     end
-
 
     it 'removes doubled new lines' do
-      %x|exe/boilerplate page test|
-      file_content = ''
-      open('test/title.md', 'r') do |file|
-        file_content = file.read()
-      end
-
-      expect(file_content).not_to match /[\r\n]{3,}[^-]*(?=-{3}$)/m
-
-    end
-
-
-    it 'handles file options' do
-      output = %x|exe/boilerplate create test -T "Test Title" --suffix .markdown --timestamp|
-      expect(Dir["test/*test-title.markdown"]).not_to be_empty
+      is_expected.not_to match /[\r\n]{3,}[^-]*(?=-{3}$)/m
     end
 
     it 'replaces the boilerplate templates {{ boilerplate.xxx }}' do
-      output = %x|exe/boilerplate create test|
-      file_content = ''
-      open('test/title.md', 'r') do |file|
-        file_content = file.read()
-      end
-      expect(file_content).not_to match /\{{2}\s*boilerplate\.\w+\s*\}{2}/
-      expect(file_content).to match "Type is testing" 
-      expect(file_content).to match "File is title.md" 
+      is_expected.not_to match /\{{2}\s*boilerplate\.\w+\s*\}{2}/
+      is_expected.to match "Type is testing" 
+      is_expected.to match "File is title.md" 
     end
-
 
   end
 
-  context 'init command' do
+  context '`boilerplate <page> -T "Test Title" --suffix .markdown --timestamp`' do
+    before do
+      %x|exe/boilerplate test -T "Test Title" --suffix .markdown --timestamp|
+    end
+
+    it 'handles file options' do
+      expect(Dir["test/*test-title.markdown"]).not_to be_empty
+    end
+  end
+
+  context '`boilerplate init` command' do
+    subject {%x|exe/boilerplate init|}
+
+    it {is_expected.not_to match /Fatal/}
+
     it 'creates example.md' do
-      output = %x|exe/boilerplate init|
-      expect(output).not_to match /Fatal/
       expect(File.exist?('_boilerplates/example.md')).to eq(true)
     end
   end
 
-  context 'list command' do
-    it 'displays a list' do
-      output = %x|exe/boilerplate list|
-      expect(output).to match('test')
-      expect(output).to match('example')
-      expect(output).not_to match( ".md")
+  context '`boilerplate list` command' do
+    subject { %x|exe/boilerplate list| }
+    it 'displays a list of boilerplate options' do
+      is_expected.to match('test')
+      is_expected.to match('example')
+      is_expected.not_to match(".md")
     end
   end
 
-  context 'alias command' do
-
-    it 'bplate' do
-      output = %x|exe/bplate|
-      expect(output).not_to match "not found"
-      expect(output).not_to match "no such file"
-    end
+  context '`bplate` alias command' do
+    subject { %x|exe/bplate| }
+    it {is_expected.not_to match "not found"}
+    it {is_expected.not_to match "no such file"}
   end
 
 end
